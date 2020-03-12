1b5c6ae9954885d4268e19fb69818ccf72919ac5
Bethany Ebel <bethanyebel@gmail.com>
2020-03-12 15:23:33 +0000

    Almost done Q1

---
+++ Q1/PlotSpline.m
@@ -0,0 +1,15 @@
+% Reads in data
+value = csvread('PlotSpline.csv');
+
+% Retrieves h and error values
+hvec = value(1,1:end-1); 
+error = value(2, 1:end-1);
+
+figure(1);
+loglog(hvec, error, 'm');
+xlabel('h'); ylabel('Error');
+title('Spline error as step-size h changes for point x=1/3');
+
+coefficients = polyfit(log(hvec), log(error), 1);
+slope = coefficients(1);
+disp(slope); % Approximately 4
--- Q1/execute.cpp
+++ Q1/execute.cpp
@@ -2,6 +2,8 @@
 #include "Function1.hpp"
 #include "AbstractFunction.hpp"
 #include<iostream>
+#include<fstream>
+#include<cmath>
 
 
 int main(int argc, char* argv[]) {
@@ -9,6 +11,7 @@ int main(int argc, char* argv[]) {
   // Question 1c and 1d
   int n = 8;
   double len = 2.0; // So h=1/4
+
 
   Function1* f1 = new Function1(); // Initialises function object
   Spline* s1 = new Spline(len,n,*f1); // Initialises spline object
@@ -20,18 +23,76 @@ int main(int argc, char* argv[]) {
   (*s1).solveTridiaognal(); // Solves system of equations
   (*s1).showCoeff();
 
+  std::cout << "\nSpline at x=0 is " << (*s1).evaluateSpline(0);
+  std::cout << "\nf(0) is " << (*f1).evaluateF(0) << std::endl;
+
+  std::cout << "\nSpline at x=0.25 is " << (*s1).evaluateSpline(0.25);
+  std::cout << "\nf(0.25) is " << (*f1).evaluateF(0.25) << std::endl;
+
   // Question 1e
+  /*
   std::cout << "\nSpline at x=0.3 is " << (*s1).evaluateSpline(0.3);
   std::cout << "\nf(0.3) is " << (*f1).evaluateF(0.3) << std::endl;
+  std::cout << (*s1).error(0.3);
 
   std::cout << "\nSpline at x=0.9 is " << (*s1).evaluateSpline(0.9);
   std::cout << "\nf(0.9) is " << (*f1).evaluateF(0.9) << std::endl;
 
   std::cout << "\nSpline at x=0.1 is " << (*s1).evaluateSpline(0.1);
   std::cout << "\nf(0.1) is " << (*f1).evaluateF(0.1) << std::endl;
+  */
+
+  // Writes to file
+  std::ofstream file;
+  file.open("Table.csv");
+  assert(file.is_open());
+
+  std::ofstream file2;
+  file2.open("PlotSpline.csv");
+  assert(file2.is_open());
+
+  file << "h," << "q," << "error" << std::endl;
+  //file << std::scientific;
+
+  double xstar = double(1)/double(3);
+  int sim = 10; // change this to get more results
+  double* hValue = new double[sim];
+  double* error = new double[sim];
+
+  for(int i=1; i<=sim; i++) {
+    hValue[i-1] = double(1)/double(pow(2,i));
+    int n = len/hValue[i-1]; // only works with specific len (otherwise n is not an integer)
+    Spline* s = new Spline(len,n,*f1);
+
+    (*s).Nodes();
+    (*s).FindSystem();
+    (*s).solveTridiaognal();
+    error[i-1] = (*s).error(xstar);
+    double approx = (*s).evaluateSpline(xstar);
+
+    file << hValue[i-1] << "," << approx << "," << error[i-1] << std::endl;
+    delete s;
+  }
+
+  for (int i=1; i<=sim; i++) {
+    file2 << hValue[i-1] << ",";
+  }
+  file2 << std::endl;
+  for (int i=1; i<=sim; i++) {
+    file2 << error[i-1] << ",";
+  }
+
+
+  file.close();
+  file2.close();
+
+  std::system("rm ../../../MATLAB/PlotSpline.csv");
+  std::system("cp PlotSpline.csv ../../../MATLAB");
 
   delete f1;
   delete s1;
+  delete[] hValue;
+  delete[] error;
 
   return 0;
 }
